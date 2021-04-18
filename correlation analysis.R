5abf8d6fb40b01f5e2c880ea7e9ce85d27383f54
Aashlesha Chekkala <82521160+Aashlesha97@users.noreply.github.com>
2021-04-19 00:10:32 +0200

    Add files via upload

---
+++ correlation analysis.R
@@ -0,0 +1,15 @@
+#import excel file with the columns to be correlated
+view(mydata)
+
+#the corr function will correlate the columns present in the excel file
+corr(mydata, use = "pairwise.complete.obs")
+
+ #the correlation information is stored in the object M
+M <- cor(mydata, use = "pairwise.complete.obs")
+
+#corrplot is used to form the correlation matrix. 
+#using different fucntions within corrplot, different forms if matrix can be formed
+
+corrplot (M, method = "circle")
+corrplot (M, typr = "upper")
+corrplot (M, type = "lower")
